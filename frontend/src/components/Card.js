d8ef1566212e22b40b479385094b8d4952777e3d
Ayush Kumar <aykr571@gmail.com>
2024-02-02 19:31:52 +0530

    FfFggg

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Card = ({ product }) => {
   return (
-    <div className="pb-3 sm:h-200">
+    <div className="pb-3 h-80 sm:h-100">
       <img
         className="w-[100%] rounded h-60"
         src={
